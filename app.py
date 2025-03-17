b9bc4054953613ce843534861ccafda025a44640
1165qwc <160745739+1165qwc@users.noreply.github.com>
2025-03-17 08:23:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import PyPDF2
-from langchain.llms import OpenAI
+from langchain_openai import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
